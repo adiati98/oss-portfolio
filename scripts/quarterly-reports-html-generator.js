7d92bdc06aefd387269a70e06122c0dd08f3b0a6
Ayu Adiati <retno.ayu98@gmail.com>
2025-11-06 14:41:13 +0100

    change the link to tailwind cdn

--- scripts/quarterly-reports-html-generator.js
+++ scripts/quarterly-reports-html-generator.js
@@ -94,8 +94,7 @@ async function writeHtmlFiles(groupedContributions) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${quarter} ${year} Open Source Contributions Report</title>
-    <!-- Load Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
 				@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
         body {
