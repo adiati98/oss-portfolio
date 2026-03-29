089cf3b440acfbfb6679d6e354aa05636a2c76ef
Ayu Adiati <retno.ayu98@gmail.com>
2026-03-29 11:38:30 +0200

    add metadata folder to list fcc articles manually

---
+++ metadata/fcc-articles.js
@@ -0,0 +1,33 @@
+/**
+ * Manual list of freeCodeCamp articles.
+ */
+module.exports = [
+  {
+    title: 'How to Set Up GitHub CLI on WSL2',
+    link: 'https://www.freecodecamp.org/news/github-cli-wsl2-guide/',
+    date: '2024-08-14',
+    platform: 'freeCodeCamp',
+    tags: ['github'],
+  },
+  {
+    title: 'How to Keep Branches Up-to-Date and Resolve Merge Conflicts in GitHub and VS Code',
+    link: 'https://www.freecodecamp.org/news/keep-branches-up-to-date-resolve-merge-conflicts/',
+    date: '2023-11-22',
+    platform: 'freeCodeCamp',
+    tags: ['opensource', 'beginner'],
+  },
+  {
+    title: 'How to Contribute to Open-Source Projects – Git & GitHub Workflow for Beginners',
+    link: 'https://www.freecodecamp.org/news/git-and-github-workflow-for-open-source/',
+    date: '2023-09-22',
+    platform: 'freeCodeCamp',
+    tags: ['opensource', 'beginners guide'],
+  },
+  {
+    title: 'How to Contribute to Open Source Projects – Non-Technical Things You Should Know',
+    link: 'https://www.freecodecamp.org/news/how-to-contribute-to-open-source/',
+    date: '2023-09-14',
+    platform: 'freeCodeCamp',
+    tags: ['opensource', 'beginners guide'],
+  },
+];
