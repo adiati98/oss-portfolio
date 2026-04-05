813b5ae810ee55c688a8cedcd211467dea0a8729
Ayu Adiati <retno.ayu98@gmail.com>
2026-04-05 23:53:46 +0200

    create metadata/workbench-messages.js for random no task phrases

---
+++ metadata/workbench-messages.js
@@ -0,0 +1,14 @@
+const WORKBENCH_SUCCESS_MESSAGES = [
+  "Hooray! You're all caught up.",
+  'The workbench is sparkling clean.',
+  'Inbox Zero achieved! Time for a coffee?',
+  'Mission accomplished. No tasks found.',
+  'All quiet on this front. Well done!',
+  'Looks like the code is behaving itself... for now.',
+  "Nothing to see here. You've cleared the deck!",
+  'Zero tasks. The ecosystem is in good hands!',
+  "You've outpaced the tasks!",
+  "404: Tasks not found (and that's a good thing!)",
+];
+
+module.exports = { WORKBENCH_SUCCESS_MESSAGES };
