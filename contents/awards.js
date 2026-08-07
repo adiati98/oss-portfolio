bec6f1e9fc0353f3d1e93f644739698aa56a722b
Ayu Adiati <45172775+adiati98@users.noreply.github.com>
2026-08-07 23:18:45 +0200

    fix: remove hypen

--- contents/awards.js
+++ contents/awards.js
@@ -15,6 +15,6 @@ module.exports = [
     url: 'https://mautic.org/blog/learnings-from-mautic-world-conference-2025/',
     highlight: true,
     description:
-      'Awarded by the Mautic community at Mautic World Conference 2025 in recognition of outstanding individual contribution and dedication to the open-source project.',
+      'Awarded by the Mautic community at Mautic World Conference 2025 in recognition of outstanding individual contribution and dedication to the open source project.',
   },
 ];
